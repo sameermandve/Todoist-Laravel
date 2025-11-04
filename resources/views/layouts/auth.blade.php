2817ccf5fb501889f9ca6bb59c9de6620c05899f
Sameer Mandve <mandvesameer@gmail.com>
2025-11-04 20:57:41 +0530

    Add Flowbite integration and update dependencies
    - Added Flowbite as a dependency in package.json and package-lock.json.
    - Updated app.css to include Flowbite styles and plugins.
    - Created a new auth.blade.php layout for authentication views.
    - Updated composer.json and composer.lock to include blade-lucide-icons.

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield("title", "Todoist | Todo App")</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="h-screen">
+        <div class="flex flex-col items-center justify-center">
+            <div class="w-full max-w-md space-y-6 text-teal-500">
+                @yield("content");
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+</body>
+
+</html>
